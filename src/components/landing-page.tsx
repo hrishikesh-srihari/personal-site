7bcd3924e6a58ae97180ca65676283bfd7012eb3
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-02-05 19:34:15 -0800

    added experiences section and rearranged projects section

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -35,6 +35,12 @@ export function LandingPage() {
           </Link>
           <Link
             className="text-sm font-medium hover:underline underline-offset-4 transition-all duration-500 ease-in-out"
+            href="#experiences"
+          >
+            Experiences
+          </Link>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4 transition-all duration-500 ease-in-out"
             href="#about"
           >
             About
@@ -73,8 +79,8 @@ export function LandingPage() {
             <h2 className="text-3xl text-gray-800 dark:text-gray-800 font-bold tracking-tighter sm:text-5xl font-space-grotesk transition-all dark:duration-500 ease-in-out">
               Projects
             </h2>
-            <div className="mx-auto grid items-start gap-10 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3 mt-8">
-              <div className="group grid gap-6 transition-all duration-200 transform hover:scale-105 pr-3">
+            <div className="mx-auto grid items-start gap-10 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-7xl lg:grid-cols-3 mt-8 mr-5">
+              <div className="group grid gap-6 transition-all duration-200 transform hover:scale-105">
                 <img
                   className="w-full h-48 object-cover rounded-md transition-transform duration-500 ease-in-out group-hover:scale-105"
                   height="200"
@@ -131,13 +137,19 @@ export function LandingPage() {
                           width="200"
                         />
                       </div>
-                      <h2>
-                        Technologies:
-                      </h2>
-                      <div className="gap-2">
-                        <Badge variant="secondary">🏆 W</Badge>
+                      <CredenzaTitle>Technologies</CredenzaTitle>
+                      <div className="flex flex-grid">
+                        <Badge variant="secondary">NextJS</Badge>
+                        <span className="px-1"></span>
+                        <Badge variant="secondary">Typescript</Badge>
+                        <span className="px-1"></span>
+                        <Badge variant="secondary">ConvexDB</Badge>
+                        <span className="px-1"></span>
+                        <Badge variant="secondary">Clerk Auth</Badge>
+                        <span className="px-1"></span>
+                        <Badge variant="secondary">Google Cloud AI</Badge>
                       </div>
-                      <Badge variant="secondary">🏆 Wo</Badge>
+                      
                       
 
                       
@@ -160,12 +172,14 @@ export function LandingPage() {
                   <Link
                     className="italic text-sm font-semibold text-gray-800 dark:text-gray-800 hover:underline underline-offset-4 transition-all duration-500 ease-in-out"
                     href="https://zotconnect.vercel.app"
+                    target="_blank"
                   >
                     Visit Site
                   </Link>
                   <Link
                     className="italic text-sm font-semibold text-gray-800 dark:text-gray-800 hover:underline underline-offset-4 font-space-grotesk transition-all duration-500 ease-in-out"
                     href="https://github.com/abhi-arya1/zotconnect"
+                    target="_blank"
                   >
                     Github
                   </Link>
@@ -193,12 +207,14 @@ export function LandingPage() {
                   <Link
                     className="italic text-sm font-semibold hover:underline underline-offset-4 text-gray-800 dark:text-gray-800 transition-all duration-500 ease-in-out"
                     href="#"
+                    target="_blank"
                   >
                     Visit Site
                   </Link>
                   <Link
                     className="italic text-sm font-semibold hover:underline underline-offset-4 text-gray-800 dark:text-gray-800 transition-all duration-500 ease-in-out"
                     href="#"
+                    target="_blank"
                   >
                     Github
                   </Link>
@@ -239,17 +255,81 @@ export function LandingPage() {
             </div>
           </div>
         </section>
-        <section className="w-full py-12 md:py-24 lg:py-32 transition-all duration-500 ease-in-out" id="about">
+        <section
+          className="w-full py-12 md:py-24 lg:py-32 transition-all duration-500 ease-in-out"
+          id="experiences"
+        >
           <div className="container px-4 md:px-6">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-space-grotesk transition-all duration-500 ease-in-out">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl transition-all duration-500 ease-in-out">
+              Experiences
+            </h2>
+            <div className="mx-auto grid items-start gap-12 sm:max-w-4xl sm:grid-cols-2 md:gap-16 lg:max-w-7xl mt-8 pl-10 mr-5">
+              <div className="group grid gap-4 transition-all duration-200 transform hover:scale-105">
+                <img
+                  className="w-full h-48 object-cover rounded-md transition-transform duration-500 ease-in-out group-hover:scale-105"
+                  height="200"
+                  src="/nasasuits.jpg.webp"
+                  style={{
+                    aspectRatio: "200/200",
+                    objectFit: "cover",
+                  }}
+                  width="200"
+                />
+                <h3 className="text-lg font-bold font-space-grotesk transition-all duration-500 ease-in-out">
+                  SWE Intern at NASA SUITS
+                </h3>
+                <p className="text-gray-500 dark:text-gray-400 font-space-grotesk transition-all duration-500 ease-in-out">
+                  A brief description of the experience.
+                </p>
+                <div className="flex gap-4">
+                  <Link
+                    className="text-sm hover:underline underline-offset-4 font-space-grotesk transition-all duration-500 ease-in-out"
+                    href="#"
+                  >
+                    Learn More
+                  </Link>
+                </div>
+              </div>
+              <div className="group grid gap-4 transition-all duration-200 transform hover:scale-105">
+                <img
+                  className="w-full h-48 object-cover rounded-md transition-transform duration-500 ease-in-out group-hover:scale-105"
+                  height="200"
+                  src="/cs_at_mines.png"
+                  style={{
+                    aspectRatio: "200/200",
+                    objectFit: "cover",
+                  }}
+                  width="200"
+                />
+                <h3 className="text-lg font-bold font-space-grotesk transition-all duration-500 ease-in-out">
+                  ComputingForGood Intern at Colorado School of Mines
+                </h3>
+                <p className="text-gray-500 dark:text-gray-400 font-space-grotesk transition-all duration-500 ease-in-out">
+                  A brief description of the experience.
+                </p>
+                <div className="flex gap-4">
+                  <Link
+                    className="text-sm hover:underline underline-offset-4 font-space-grotesk transition-all duration-500 ease-in-out"
+                    href="#"
+                  >
+                    Learn More
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="w-full py-12 bg-violet-400 dark:bg-violet-400 md:py-24 lg:py-32 transition-all duration-500 ease-in-out" id="about">
+          <div className="container px-4 md:px-6">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl transition-all duration-500 ease-in-out">
               About Me
             </h2>
             <div className="flex flex-col gap-4 mt-8">
-              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400 font-space-grotesk transition-all duration-500 ease-in-out">
+              <p className="max-w-[600px] text-gray-700 md:text-xl dark:text-gray-700 font-space-grotesk transition-all duration-500 ease-in-out">
                 I am a full-stack developer with over 5 years of experience in building web applications. I specialize
                 in JavaScript and have professional experience working with React and Node.js.
               </p>
-              <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400 font-space-grotesk transition-all duration-500 ease-in-out">
+              <p className="max-w-[600px] text-gray-700 md:text-xl dark:text-gray-700 font-space-grotesk transition-all duration-500 ease-in-out">
                 I have a passion for learning and sharing my knowledge with others as clearly as possible. If you found
                 value in something I have created, please feel free to share, comment, and ask me anything!
               </p>
@@ -257,8 +337,8 @@ export function LandingPage() {
           </div>
         </section>
       </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-violet-400 dark:bg-violet-400 transition-all duration-500 ease-in-out">
-        <p className="text-xs text-gray-800 dark:text-gray-800 font-space-grotesk transition-all duration-500 ease-in-out">
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t transition-all duration-500 ease-in-out">
+        <p className="text-xs text-gray-300 dark:text-gray-300 font-space-grotesk transition-all duration-500 ease-in-out">
           © 2024 Rishi Srihari. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
